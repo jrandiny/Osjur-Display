ba7d302821c9f4b49c01ffc3f8d9b178d88a629f
jrandiny <jrandiny2@gmail.com>
2018-07-06 22:25:19 +0700

    Add auto refresh

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ function getToken(){
    })
    .done(function( msg ) {
      $(".boks p").html(msg);
+
+     var currTime = new Date(); // for now
+
+     var waitTime = (5-(currTime.getMinutes()%5))*60
+     waitTime = waitTime - currTime.getSeconds();
+
+     setTimeout(getToken(),waitTime*1000);
+
    }).fail(function( jqXHR, textStatus ) {
      alert( "Error: " + textStatus + " / " + jqXHR.statusText);
    });
